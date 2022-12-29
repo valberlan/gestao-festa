6b8dfdcbb67f0adef22f89de40954756d90ee69b
Valberlan Silva <valberlan.silva@unemat.br>
2022-12-29 10:37:25 -0300

    Primeiro commit.

---
+++ src/main/java/com/algaworks/festa/config/InMemorySecurityConfig.java
@@ -0,0 +1,23 @@
+package com.algaworks.festa.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+
+@Configuration
+public class InMemorySecurityConfig {
+
+	@Autowired	
+	public void configureGlobal(AuthenticationManagerBuilder builder) throws Exception {
+		builder
+			.inMemoryAuthentication()
+			.withUser("user").password("{noop}password").roles("USER")
+			.and()
+			.withUser("joao").password("123").roles("USER")
+			.and()
+			.withUser("alexandre").password("123").roles("USER")
+			.and()
+			.withUser("thiago").password("123").roles("USER");
+	}
+
+}
---
+++ src/main/java/com/algaworks/festa/model/Convidado.java
@@ -0,0 +1,53 @@
+package com.algaworks.festa.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class Convidado implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5326055725090862761L;
+	
+	@Id
+	@GeneratedValue(generator = "increment")
+	@GenericGenerator(name = "increment", strategy = "increment")
+
+	private Long id;
+	
+	private  String nome;
+	
+	private Integer quantidadeAcompanhantes;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getQuantidadeAcompanhantes() {
+		return quantidadeAcompanhantes;
+	}
+
+	public void setQuantidadeAcompanhantes(Integer quantidadeAcompanhantes) {
+		this.quantidadeAcompanhantes = quantidadeAcompanhantes;
+	}
+
+}
